de6bbdc8b34ee1aa9730bb7312af964798f9190e
Your New Username <abuzarkhan2486@gmail.com>
2026-05-24 15:29:36 +0530

    basic

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,15 @@
-import express from "express"
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+app.get("/home",(req,res)=>{
+    res.send("hello from home ")
+})
 
 
 
