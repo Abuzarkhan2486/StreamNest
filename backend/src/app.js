dbc2783c6925bb8695f15ae6744e5a342dcba7ec
Your New Username <abuzarkhan2486@gmail.com>
2026-05-26 23:55:45 +0530

    add

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,18 @@
-import dotenv from "dotenv";
-dotenv.config();
-import express from "express";
+require("dotenv").config()
+const express = require("express")
+const authRoute= require("./routes/authRoute.js")
+const productRoute= require("./routes/productRoute.js")
+const cookieParser = require("cookie-parser")
 const app = express()
 
+
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extend:true}))
+app.use(cookieParser())
 
 
-app.get("/home",(req,res)=>{
-    res.send("hello from home ")
-})
+app.use("/api/auth",authRoute)
+app.use("/api/product",productRoute)
 
 
-
-
-
-export default app
+module.exports = app
