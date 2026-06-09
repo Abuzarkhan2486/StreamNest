33aaeac67f216eda01d62104e7d701f23fb8168f
Your New Username <abuzarkhan2486@gmail.com>
2026-06-09 23:17:41 +0530

    make controller

---
+++ backend/src/controller/authController.js
@@ -0,0 +1,68 @@
+const userModel = require("../models/user.model")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const { asyncRapper } = require("../utils/asyncHandleer")
+
+const registerController=asyncRapper(async(req,res)=>{
+    
+         let {name,email,password,}=req.body
+
+         if(!name|| !email || !password){
+            res.status(400).json({
+                success:false,
+                message:"all feilds are required"
+
+            })
+         }
+
+         let  isexisteduser= await userModel.findOne({email})
+
+         if(isexisteduser){
+
+           return  res.status(400).json({
+                success:false,
+                message:"users already existed"
+
+            })
+         }
+
+         let hashpass= await bcrypt.hash(password,10)
+
+         let newuser=await  userModel.create({
+            name,
+            email,
+            password:hashpass
+         })
+
+         let token = jwt.sign({id:newuser._id},process.env.JWT_SECRET,{expiresIn:"1h"})
+         res.cookie("token",token)
+
+
+         return res.status(200).json({
+            message:"user crerated",
+            user:newuser
+
+         })
+
+         
+     
+     
+        console.log(error);
+        
+        return res.status(500).json({
+            message:"internal server error ",
+            success:false
+        })
+        
+    
+    })
+
+
+
+
+
+
+module.exports={
+    registerController,
+    
+}
