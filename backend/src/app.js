2f7d21f7ec333b2c2d9398eaef212f46caa55db6
Your New Username <abuzarkhan2486@gmail.com>
2026-06-10 14:43:01 +0530

    change to industrylevel

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,11 @@
-require("dotenv").config()
-const express = require("express")
-const authRoute= require("./routes/authRoute.js")
-const productRoute= require("./routes/productRoute.js")
-const cookieParser = require("cookie-parser")
+import express from "express"
+
 const app = express()
 
 
-app.use(express.json())
-app.use(express.urlencoded({extend:true}))
-app.use(cookieParser())
 
 
-app.use("/api/auth",authRoute)
-app.use("/api/product",productRoute)
 
 
-module.exports = app
+
+export  {app}
